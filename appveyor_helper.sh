b5c5f2f4e7b06cd0635ee7a2aaaa8a9093213324
hiroyuki nagata <newserver002@gmail.com>
2014-11-29 01:36:50 +0900

    Fix

--- appveyor_helper.sh
+++ appveyor_helper.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 FREEGLUT_URL=https://github.com/dcnieho/FreeGLUT/archive/FG_2_8_0.tar.gz
+FREEGLUT_TAR=FG_2_8_0.tar.gz
+FILE_UNTAR=FreeGLUT-FG_2_8_0/freeglut
 
-mkdir -p /usr/local/src                        || exit 1
-cd /usr/local/src	                       || exit 1
-wget --no-check-certificate ${FREEGLUT_URL}    || exit 1 	
-tar xvf FG_2_8_0.tar.gz	                       || exit 1 	
-cd FreeGLUT-FG_2_8_0/freeglut                  || exit 1 	
-./autogen.sh		                       || exit 1 	
-./configure --prefix=/mingw                    || exit 1 	
-make			                       || exit 1 	
-make install		                       || exit 1 	
+mkdir -p /usr/local/src                                           || exit 1
+cd /usr/local/src	                                          || exit 1
+wget --no-check-certificate ${FREEGLUT_URL} -O ${FREEGLUT_TAR}    || exit 1 	
+tar xvf ${FREEGLUT_TAR}	                                          || exit 1 	
+cd ${FILE_UNTAR}                                                  || exit 1
+./autogen.sh		                                          || exit 1 	
+./configure --prefix=/mingw                                       || exit 1 	
+make			                                          || exit 1 	
+make install		                                          || exit 1 	
 
 
